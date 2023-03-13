2cc7fd5a738c44a92515e670370c52568942315f
Ericles Brum <ericlespbrum@gmail.com>
2023-03-13 15:43:56 -0300

    implementando logica pt1

--- src/main/Calculator.js
+++ src/main/Calculator.js
@@ -1,19 +1,42 @@
+import { useState } from 'react';
 import Button from '../components/Button/Button';
 import Display from '../components/Display/Display';
 import './Calculator.css';
 
-const clearMemory = () => {
-}
-const setOperation = (operation) => {
-    console.log(operation);
-}
-const addDigit = (number) => {
-    console.log(number);
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
 }
 const Calculator = () => {
+    const [state, setState] = useState(initialState)
+    const clearMemory = () => {
+        setState({ ...initialState })
+    }
+    const setOperation = (operation) => {
+    }
+    const addDigit = (number) => {
+        if (number === '.' && state.displayValue.includes('.')) {
+            return;
+        }
+        const clearDisplay = state.displayValue == '0' || state.clearDisplay;
+        const currentValue = clearDisplay ? '' : state.displayValue;
+        const displayValue = currentValue + number;
+        const values = state.values;
+
+        if (number !== '.') {
+            const i = state.current;
+            const newValue = parseFloat(displayValue);
+            values[i] = newValue;
+        }
+        setState({ ...state, displayValue, clearDisplay: false, values })
+        console.log(state)
+    }
     return (
         <div className='calculator'>
-            <Display value={100} />
+            <Display value={state.displayValue} />
             <Button label={'AC'} Click={clearMemory} typeButton={' triple'} />
             <Button label={'/'} Click={setOperation} typeButton={' operation'} />
             <Button label={7} Click={addDigit} />
@@ -29,7 +52,7 @@ const Calculator = () => {
             <Button label={3} Click={addDigit} />
             <Button label={'+'} Click={setOperation} typeButton={' operation'} />
             <Button label={0} Click={addDigit} typeButton={' double'} />
-            <Button label={'.'} Click={setOperation} />
+            <Button label={'.'} Click={addDigit} />
             <Button label={'='} Click={setOperation} typeButton={' operation'} />
         </div>
     )
